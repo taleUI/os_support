b407c32822ef3229a836c09de0db28844c81c196
theVakhovskeIsTaken <thevakhovske@petalmail.com>
2023-10-20 12:57:54 +0000

    Download fixups

--- gh-update.sh
+++ gh-update.sh
@@ -10,13 +10,13 @@ downloadpath=/home/.gh_offload/updatecontainer
 get_img_details(){
     stdout=$(jq 'sort_by(.created_at) | reverse')
     stdout=$(echo "${stdout}" | jq 'del(.[] | select(.assets[].state != "uploaded"))')
-    download_img_url=$(echo "${stdout}" | jq -r '[ .[] | select(.prerelease==false) ] | first | .assets[] | select(.browser_download_url | test("img")) | .browser_download_url' | head -n1)
-    download_sha256_url=$(echo "${stdout}" | jq -r '[ .[] | select(.prerelease==false) ] | first | .assets[] | select(.browser_download_url | test("img")) | .browser_download_url' | tail -n1)
     os_tag_name=$(echo "${stdout}" | jq -er '[ .[] | select(.prerelease==false) ] | first | .tag_name')
+    download_img_id=$(echo "${stdout}" | jq -er '[ .[] | select(.prerelease==false) ] | first | .assets[] | .url' | head -n1)
+    download_sha_id=$(echo "${stdout}" | jq -er '[ .[] | select(.prerelease==false) ] | first | .assets[] | .url' | tail -n1)
     if [[ "${os_tag_name}" == "$(cat /etc/os-release | grep VARIANT_ID | cut -d '=' -f 2)" ]]; then
         echo "System up to date."
     else
-        echo -e "OS_TAG_NAME=$os_tag_name\nDL_IMG=$download_img_url\nDL_SHA=$download_sha256_url" > ${argdata}
+        echo -e "OS_TAG_NAME=$os_tag_name\nDL_IMG=$download_img_url\nDL_SHA=$download_sha256_url\nGH_IMG_DL=$download_img_id\nGH_SHA_DL=$download_sha_id" > ${argdata}
         echo "Update available. OS Update: ${os_tag_name}"
     fi
 }
@@ -39,30 +39,75 @@ updatecheck(){
     fi
 }
 
+verintg(){
+    echo "Verifying update file integrity..."
+    CURR_SHA=$(echo $(sha256sum ${downloadpath}/${OS_TAG_NAME}/${OS_TAG_NAME}.img.zst) | awk '{print $1}')
+    if [[ "${CURR_SHA}" == "$(cat ${downloadpath}/${OS_TAG_NAME}/${OS_TAG_NAME}.img.zst.sha256)" ]]; then
+        touch ${ready_watcher}
+        echo "Update file download complete. The system is ready to update."
+    else
+        rm -rf ${downloadpath}/${OS_TAG_NAME}
+        echo "Verification failed. Download contents erased."
+        exit 1
+    fi
+}
+
+dlupd(){
+    source ${argdata}
+    if [[ -f "${downloadpath}/${OS_TAG_NAME}/${OS_TAG_NAME}.img.zst" ]] && [[ -f "${downloadpath}/${OS_TAG_NAME}/${OS_TAG_NAME}.img.zst.sha256" ]]; then
+        verintg
+    else
+        mkdir -p ${downloadpath}/${OS_TAG_NAME}
+        echo "Downloading update files..."
+        if [[ "${releasepath}" =~ "int" ]]; then
+            if [[ -f "/etc/gh-update-token" ]]; then
+                curl --http1.1 -L -H "Accept: application/octet-stream" -H "Authorization: Bearer $(cat /etc/gh-update-token)" -o ${downloadpath}/${OS_TAG_NAME}/${OS_TAG_NAME}.img.zst "${GH_IMG_DL}"
+                curl --http1.1 -L -H "Accept: application/octet-stream" -H "Authorization: Bearer $(cat /etc/gh-update-token)" -o ${downloadpath}/${OS_TAG_NAME}/${OS_TAG_NAME}.img.zst.sha256 "${GH_SHA_DL}"
+            else
+                echo -e "You are on an internal build without an authorization to\nthe update endpoint.\nPlease pipe your valid Github token to /etc/gh-update-token via echo."
+                exit 0
+            fi
+        else
+            curl --http1.1 -L -H "Accept: application/octet-stream" -o ${downloadpath}/${OS_TAG_NAME}/${OS_TAG_NAME}.img.zst "${GH_IMG_DL}"
+            curl --http1.1 -L -H "Accept: application/octet-stream" -o ${downloadpath}/${OS_TAG_NAME}/${OS_TAG_NAME}.img.zst.sha256 "${GH_SHA_DL}"
+        fi
+        verintg
+    fi
+}
+
 if [ -n "$1" ]; then
     case "$1" in
     "check")
+        rm -f ${argdata} ${ready_watcher}
 	    updatecheck
         ;;
     "download-update")
         if [[ -f "${argdata}" ]]; then
             dlupd
         else
-            updatecheck
-            if [[ -f "${argdata}" ]]; then
-                dlupd
-            fi
+            echo "Update arguments are not yet available. Please try checking for updates first."
+            exit 1
         fi
         ;;
     "apply-now")
+        if [[ -f "${ready_watcher}" ]]; then
+            gh-update-os --apply ${argdata}
+            exit 7
+        fi
         if [[ -f "${argdata}" ]]; then
             dlupd
             if [[ -f "${ready_watcher}" ]]; then
                 gh-update-os --apply ${argdata}
+                exit 7
             fi
         else
             echo -e "No update arguments set. Checking for updates...\n"
             updatecheck
+            dlupd
+            if [[ -f "${ready_watcher}" ]]; then
+                gh-update-os --apply ${argdata}
+                exit 7
+            fi
         fi
         ;;
     "-d")
